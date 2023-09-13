6e2090d44812c897cf061c6e32f62332c16f4b63
Kjszywala <kamil.szywala@magicard.com>
2023-09-13 11:26:26 +0100

    Added model class to console app

---
+++ Challenge.ConsoleApp/Models/PostCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge.ConsoleApp.Models
+{
+    public class PostCode
+    {
+        public string? Postcode { get; set; }
+        public int? Eastings { get; set; }
+        public int? Northings { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public string? Town { get; set; }
+        public string? Region { get; set; }
+        public string? Country { get; set; }
+        public string? CountryString { get; set; }
+    }
+}
